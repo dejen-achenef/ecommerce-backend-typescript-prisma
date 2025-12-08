1aed5971e40118d42bfa3267192caa0360df91c6
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-09 00:13:56 +0300

    style: improve error handler spacing

--- functions/errorHandler.ts
+++ functions/errorHandler.ts
@@ -14,6 +14,7 @@ export class AppError extends Error {
 }
 
 export const handlePrismaError = (error: any): AppError => {
+
   if (error instanceof Prisma.PrismaClientKnownRequestError) {
     switch (error.code) {
       case "P2002":

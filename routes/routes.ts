950e087ae733e20538fa8df22a689e71a5f6bb3d
dejen-achenef <dejen.achenef-ug@aau.edu.et>
2025-12-08 08:47:04 +0300

    Update routes with product and order endpoints

--- routes/routes.ts
+++ routes/routes.ts
@@ -1,14 +1,22 @@
 import express from "express";
-import { createUser } from "../functions/routeFucntions.js";
+import { createUser } from "../controllers/controllers.js";
+import { createProduct, getProducts, getProductById } from "../controllers/productControllers.js";
+import { createOrder, getOrders } from "../controllers/orderControllers.js";
+import { authenticateUser } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-// Register routes
-router.post("/auth/register", (req, res, next) => {
-  console.log("Signup route hit - Request received");
-  console.log("Body:", req.body);
-  createUser(req, res).catch(next);
-});
+// Auth routes
+router.post("/auth/register", createUser);
+
+// Product routes
+router.post("/products", authenticateUser, createProduct);
+router.get("/products", getProducts);
+router.get("/products/:id", getProductById);
+
+// Order routes
+router.post("/orders", authenticateUser, createOrder);
+router.get("/orders", authenticateUser, getOrders);
 
 // Test route to verify router is working
 router.get("/test", (req, res) => {
